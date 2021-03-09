164e2b5095d8c4a661112800fdd54de709d0bc8b
Babu Saginadham <babu.saginadham@gmail.com>
2021-03-09 20:10:18 +0530

    Add files via upload

---
+++ src/main/java/com/app/jdbc/model/Student.java
@@ -0,0 +1,47 @@
+package com.app.jdbc.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@Entity
+public class Student {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="SNO")
+	Long sno;
+	
+	@Column(name="SNAME")
+	String sname;
+	
+	//create
+	
+	//createBy
+	
+	//lockingVersion number
+	
+	//updated
+	
+	//updateBy
+	
+	//...
+	
+	public Long getSno() {
+		return sno;
+	}
+	public void setSno(Long sno) {
+		this.sno = sno;
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	
+}
